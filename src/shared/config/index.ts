9c84586f9f7b54e57020aaa7eae451705ff9aab1
DovranA <atadov23@gmail.com>
2024-12-25 17:47:29 +0500

    [fix]:base url

--- src/shared/config/index.ts
+++ src/shared/config/index.ts
@@ -1,3 +1,3 @@
 // export const API_URL = "https://dev.tmbiz.info/api";
 // export const API_URL = "http://192.168.0.107:5000/api";
-export const API_URL = "/api/admin";
+export const API_URL = import.meta.env.VITE_BASE_URL;
